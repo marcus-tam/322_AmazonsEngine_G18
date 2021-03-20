8e26583261cbf61a87b9c8eb8bc95697a07b7c0f
john-wen <43253060+john-wen@users.noreply.github.com>
2021-03-19 23:34:07 -0700

    shhhhh

--- src/main/java/ubc/cosc322/COSC322Test.java
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -2,6 +2,7 @@
 package ubc.cosc322;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -25,15 +26,17 @@ public class COSC322Test extends GamePlayer{
 	
     private String userName = null;
     private String passwd = null;
- 
+
     private int colour = 2; //0 = black 1 = white
-	
+    
+    // 0 = empty 1 = white queen 2 = black queen 3 = arrow
+    private ArrayList<Integer> gameBoard;
     /**
      * The main method
      * @param args for name and passwd (current, any string would work)
      */
     public static void main(String[] args) {				 
-    	COSC322Test player = new COSC322Test("asssrgssss[0]", "argssss[1]");
+    	COSC322Test player = new COSC322Test(args[0], args[1]);
     	
     	if(player.getGameGUI() == null) {
     		player.Go();
@@ -56,9 +59,10 @@ public class COSC322Test extends GamePlayer{
     public COSC322Test(String userName, String passwd) {
     	this.userName = userName;
     	this.passwd = passwd;
-    	
+
     	//To make a GUI-based player, create an instance of BaseGameGUI
     	//and implement the method getGameGUI() accordingly
+    	
     	this.gamegui = new BaseGameGUI(this);
     }
  
@@ -66,57 +70,44 @@ public class COSC322Test extends GamePlayer{
 
     @Override
     public void onLogin() {
-	List<Room> rooms = getGameClient().getRoomList();
-	for (Room room : rooms){
-		System.out.println(room);
-	}
-	System.out.println("Client Logged In");
-	//	System.out.println(getGameClient().getRoomList());
-//    getGameClient().joinRoom("Okanagan Lake");
-	//System.out.println(userName);
-	userName = gameClient.getUserName();
-	
-	if(gamegui != null) {
-	gamegui.setRoomInformation(gameClient.getRoomList());
-	}
+    	userName = gameClient.getUserName();
+    	
+    	if(gamegui != null) {
+    		gamegui.setRoomInformation(gameClient.getRoomList());
+    	}
 
-
+    	//int[] test = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0};
+    	//System.out.println(test.length);
     }
 
 
-    @Override
+    @SuppressWarnings("unchecked")
+	@Override
     public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
-    	//This method will be called by the GameClient when it receives a game-related message
-    	//from the server.
-	
-    	//For a detailed description of the message types and format, 
-    	//see the method GamePlayer.handleGameMessage() in the game-client-api document.
-    	
-//		if(messageType.equals("cosc322.game-state.board")){
-//			this.getGameGUI().setGameState((ArrayList<Integer>) msgDetails.get("game-state"));
-//		}
 
-//		System.out.println(msgDetails); 
-//		System.out.println((ArrayList<Integer>) msgDetails.get("game-state"));
-
-//		if(messageType.equals(GameMessage.GAME_STATE_BOARD)){
-//			
-//		}
 		if(messageType.equals(GameMessage.GAME_ACTION_MOVE)){
-			System.out.println("GAME_ACTION_MOVE");
-			this.getGameGUI().setGameState((ArrayList<Integer>) msgDetails.get("game-state"));
-			System.out.println("_________ END ___________");
+			System.out.println("------------- GAME_ACTION_MOVE -------------");
+			
+			ArrayList<Integer> queenPosCurrent = (ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR);
+			ArrayList<Integer> queenPosNew = (ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.Queen_POS_NEXT);
+			ArrayList<Integer> arrowPos = (ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.ARROW_POS);
+			
+			System.out.println(queenPosCurrent);
+			System.out.println(queenPosNew);
+			System.out.println(arrowPos);
+			
+			gamegui.updateGameState(queenPosCurrent, queenPosNew, arrowPos);
+			
+			System.out.println("-----------------------------------------");
 			
 		}
+		
 		if(messageType.equals(GameMessage.GAME_ACTION_START)) {
-			System.out.println("GAME_ACTION_START");
-//	    	gameClient.sendTextMessage("HEHEHEHEHE");
-			/*
-			 * Extract player colour
-			 */
+			System.out.println("------------- GAME_ACTION_START -------------");
+
 			if (msgDetails.get("player-white").equals(gameClient.getUserName())){
-				this.colour = 1;
-				System.out.println(gameClient.getUserName() + " plays as White");
+				System.out.println("Playing as White");
+				
 				ArrayList<Integer> queenPosCurrent = new ArrayList<>();
 				queenPosCurrent.add(4);
 				queenPosCurrent.add(10);
@@ -126,59 +117,23 @@ public class COSC322Test extends GamePlayer{
 				ArrayList<Integer> arrowPos = new ArrayList<>();
 				arrowPos.add(4);
 				arrowPos.add(4);
-
-				this.getGameClient().sendMoveMessage(queenPosCurrent, queenPosNew, arrowPos);
-				this.getGameGUI().setGameState((ArrayList<Integer>) msgDetails.get("game-state"));
-			}else {
-				this.colour = 0;
-				System.out.println(gameClient.getUserName() + " plays as Black");
 				
-				ArrayList<Integer> queenPosCurrent = new ArrayList<>();
-				queenPosCurrent.add(7);
-				queenPosCurrent.add(10);
-				ArrayList<Integer> queenPosNew = new ArrayList<>();
-				queenPosNew.add(7);
-				queenPosNew.add(2);
-				ArrayList<Integer> arrowPos = new ArrayList<>();
-				arrowPos.add(7);
-				arrowPos.add(4);
-				System.out.println("BEGIN MOVE");
-				this.getGameClient().sendMoveMessage(queenPosCurrent, queenPosNew, arrowPos);
-				System.out.println("FINISH MOVE");
-				this.getGameGUI().updateGameState(queenPosCurrent, queenPosNew, arrowPos);
-
+				gameClient.sendMoveMessage(queenPosCurrent, queenPosNew, arrowPos);
+				
+				gamegui.updateGameState(queenPosCurrent, queenPosNew, arrowPos);
+			}else {
+				System.out.println("Playing as Black");
 			}
 			
-			this.getGameGUI().updateGameState(msgDetails);
-			System.out.println("_________ END ___________");
+			System.out.println("-----------------------------------------");
 
 		}
 		if(messageType.equals(GameMessage.GAME_STATE_BOARD)) {
-			System.out.println("GAME_STATE_BOARD");
-			System.out.println(msgDetails);
-			this.getGameGUI().setGameState((ArrayList<Integer>) msgDetails.get("game-state"));
-			System.out.println("_________ END ___________");
-		}
-		if(messageType.equals(GameMessage.GAME_STATE_JOIN)) {
-			System.out.println("GAME_STATE_JOIN");
-			this.getGameGUI().updateGameState(msgDetails);
-			System.out.println("_________ END ___________");
-		}
-		if(messageType.equals(GameMessage.GAME_STATE_PLAYER_LOST)) {
-			System.out.println("GAME_STATE_PLAYER_LOST");
-			this.getGameGUI().updateGameState(msgDetails);
-			System.out.println("_________ END ___________");
-		}
-		if(messageType.equals(GameMessage.GAME_TEXT_MESSAGE)) {
-			System.out.println("GAME_TEXT_MESSAGE");
-			this.getGameGUI().updateGameState(msgDetails);
-			System.out.println("_________ END ___________");
-		}
-		if(messageType.equals(AmazonsGameMessage.ARROW_POS)) {
-			System.out.println("ARROW_POS");
-			System.out.println(msgDetails);
-			this.getGameGUI().updateGameState(msgDetails);
-			System.out.println("_________ END ___________");
+			System.out.println("------------- GAME_STATE_BOARD -------------");
+			gameBoard = (ArrayList<Integer>)msgDetails.get("game-state");
+			
+			gamegui.setGameState(gameBoard);
+			System.out.println("-----------------------------------------");
 		}
 		
     	return true;
---
+++ src/main/java/ubc/cosc322/SearchTree.java
@@ -0,0 +1,11 @@
+package ubc.cosc322;
+
+import java.util.Map;
+
+public class SearchTree {
+	
+	public static void run(Map<String, Object> gameMessage) {
+		
+	}
+
+}
